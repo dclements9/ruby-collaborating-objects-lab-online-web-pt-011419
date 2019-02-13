3e7add175f86c0aef492f0059df5f203ba3aa885
Dylan Clements <dclements@protonmail.com>
2019-02-13 21:33:58 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+  def files
+    Dir.entries(path).select {|entry| entry.include?(".mp3")}
+    #binding.pry
+  end
+  def import
+    files.each {|filename| Song.new_by_filename(filename)}
+  end
+end
